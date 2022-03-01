abed4fe088a61b4c7c90b07446af9556756d860b
Shyqeri Tari <63459350+ShyqeriTari@users.noreply.github.com>
2022-03-01 14:56:22 +0100

    Starting point

---
+++ src/server.js
@@ -0,0 +1,18 @@
+import express from "express" 
+import listEndpoints from "express-list-endpoints"
+import usersRouter from "./services/authors/index.js"
+
+
+const server = express()
+
+const port = 3001
+
+server.use(express.json()) 
+
+server.use("/authors", usersRouter)
+
+console.table(listEndpoints(server))
+
+server.listen(port, () => {
+  console.log(`Server listening on port ${port}`)
+})
